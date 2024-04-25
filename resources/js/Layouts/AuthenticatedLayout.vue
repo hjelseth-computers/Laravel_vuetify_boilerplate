b290fa9a9fd4457f451ad921967144d551f849eb
Tommy Evertsen <tommyevertsen52@gmail.com>
2024-04-25 13:35:19 +0200

    Install laravel, vue, breeze and vuetify

---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <v-app>
+        <v-app-bar :elevation="2">
+            <template v-slot:prepend>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            </template>
+
+            <v-app-bar-title>Application Bar</v-app-bar-title>
+        </v-app-bar>
+
+        <!-- Page Content -->
+        <main class="my-auto">
+            <slot name="main" />
+        </main>
+
+    </v-app>
+
+
+</template>
